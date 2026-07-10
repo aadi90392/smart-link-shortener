8ad8726c1f1a496243c15678929d3788796cada5
aadi90392 <adi375461@gmail.com>
2026-07-10 16:48:19 +0530

    added morgan logger

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, {Request, Response} from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 import authRoutes from './routes/auth.routes';
 import linkRoutes from './routes/link.routes';
 
@@ -7,6 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'))
 
 app.use('/api/auth', authRoutes);
 app.use('/api/links', linkRoutes);
